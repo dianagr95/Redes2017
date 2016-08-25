1be4a6515db32875b872e04185d951b751834814
Claudia <claugaro@ciencias.unam.mx>
2016-08-24 22:07:15 -0500

    Agrega constantes

--- Constants/Constants.py
+++ Constants/Constants.py
@@ -1,4 +1,13 @@
 #! /usr/bin/env python
 
-#TODO
+"""***********************
+Clase que contiene a las constantes.
+***********************"""
+
+class Constants(object):
+	LOCALHOST = "localhost"
+	CHAT = "chat"
+	CHAT_PORT = 5000
+
+
 
